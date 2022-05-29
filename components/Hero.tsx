7864c29d3e2149638c50a0e0f59a3c4c19eb5db7
Sean OReilly <soreilly424@gmail.com>
2022-05-29 12:40:53 -0500

    Updated Hero Sizing on all devices

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <section className="relative h-[45rem] w-full lg:h-[50rem]">
+    <section className="relative h-[45rem] w-full lg:h-[47rem]">
       <div id="overlay" className="bg-black/40 absolute inset-0 z-10" />
       <Image
         className="object-cover"
@@ -11,19 +11,19 @@ export default function Hero() {
         layout="fill"
         priority
       />
-      <div className="absolute left-0 top-0 right-0 bottom-20 z-20 flex flex-col gap-20 items-center justify-center max-w-xl mx-auto">
+      <div className="absolute left-0 top-0 right-0 bottom-20 z-20 flex flex-col gap-20 items-center justify-center mx-auto max-w-xs sm:max-w-xl md:max-w-4xl">
         <div className="text-center text-white grid gap-10">
-          <h1 className='text-6xl font-serif'>Main Keywords of What They Do, And Location If Needed</h1>
-          <p className='text-2xl font-light text-neutral-superlight leading-relaxed'>
+          <h1 className='h1'>Main Keywords of What They Do, And Location If Needed</h1>
+          <p className='p text-neutral-superlight leading-relaxed'>
             Talk about the other services they do with keywords and locations
             where they serve. Try to make this 2-3 lines tall.
           </p>
         </div>
         <div className="flex flex-col items-center justify-center gap-8 sm:flex-row">
-          <button className="px-12 py-5 font-bold text-xl bg-primary text-neutral">
+          <button className="button bg-primary text-white">
             Get Started Today
           </button>
-          <button className="px-12 py-5 font-bold text-xl bg-neutral text-white">
+          <button className="button bg-neutral text-white">
             Discover More
           </button>
         </div>
