f62668b3869c0422a6793fa792b6192bdaf55ed5
Sean OReilly <soreilly424@gmail.com>
2022-05-30 13:55:56 -0500

    Updated Contact Us Page

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -6,7 +6,7 @@ import CtaGridCenter from '@/components/CtaGridCenter'
 import { directors } from 'lib/seeds'
 import Headline from '@/components/Headline'
 import TextSideBySide from '@/components/TextSideBySide'
-import Image from 'next/image'
+import ContactForm from '@/components/ContactForm'
 
 const Projects: NextPage = () => {
   return (
@@ -18,69 +18,7 @@ const Projects: NextPage = () => {
         subheader=""
         ctaText=""
       />
-      <section className="bg-neutral-superlight py-10">
-        <div className="flex m-5 sm:m-20 sm:px-base">
-          <div className="text-center flex flex-col gap-4 bg-white h-full p-5 sm:p-20">
-            <h2 className="h2 mb-4">Contact Us</h2>
-            <form className="flex flex-col gap-4">
-              <div className="flex gap-4">
-                <input
-                  className="input"
-                  type="text"
-                  required
-                  placeholder="Name*"
-                />
-                <input
-                  className="input"
-                  type="email"
-                  required
-                  placeholder="Email*"
-                />
-              </div>
-              <div className="flex gap-4">
-                <input
-                  className="input"
-                  type="text"
-                  required
-                  placeholder="Phone*"
-                />
-                <input
-                  className="input"
-                  type="text"
-                  required
-                  placeholder="Location*"
-                />
-              </div>
-              <label className="flex p text-sm pt-4" htmlFor="message">
-                Your Message
-              </label>
-              <textarea
-                className="input"
-                name="message"
-                id=""
-                cols={20}
-                rows={5}
-              ></textarea>
-              <button
-                className="button bg-neutral text-white mt-8 rounded-md"
-                type="submit"
-              >
-                Contact Us
-              </button>
-            </form>
-          </div>
-          <div className="hidden lg:block bg-blue-300 w-[40rem]">
-            <div className="relative h-full w-full">
-              <Image
-                className="object-cover"
-                src="https://images.unsplash.com/photo-1597919304146-a8a81c30d502?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
-                alt="Contact Us Form Image"
-                layout="fill"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      <ContactForm />
       <TextSideBySide />
       <CtaOverlay />
       <Headline header="Our Directors" />
